271dc3a06bee94239192343752c5c8b6b6439c00
Berkant Yüksektepe <61010367+Berkantyuks@users.noreply.github.com>
2022-08-04 02:51:22 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+public class TCMS {
+    public static void main(String[] args) {
+      
+        String[] TC_U
+          = {"Online"};
+        String[] TC_A
+          = {"Online"};
+        String[] TC_C
+          = {"Online"};
+        String[] TC_F
+          = {"Online"};
+       
+    }
+}
